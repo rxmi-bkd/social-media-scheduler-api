77219b72d7be82f1e4af479fa0e9479ef42060be
rxmi-bkd <boukadidarami0@gmail.com>
2026-07-24 22:06:08 +0200

    save

--- src/main/java/org/bkd/social_media_scheduler_api/authentication/frameworks/SecurityConfiguration.java
+++ src/main/java/org/bkd/social_media_scheduler_api/authentication/frameworks/SecurityConfiguration.java
@@ -1,39 +1,7 @@
 package org.bkd.social_media_scheduler_api.authentication.frameworks;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFilter;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
-
-@Configuration
-@EnableWebSecurity
 public class SecurityConfiguration {
 
-  private final AuthenticationFilter authenticationFilter;
-
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-
-    httpSecurity
-
-        .csrf(AbstractHttpConfigurer::disable)
-
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
-        .authorizeHttpRequests(auth -> auth.requestMatchers("/public/**")
-                                           .permitAll()
-                                           .requestMatchers("/admin/**")
-                                           .hasRole("ADMIN")
-                                           .anyRequest()
-                                           .authenticated())
-
-        .addFilterBefore(authenticationFilter, LogoutFilter.class);
-
-    return httpSecurity.build();
-  }
+  public final static String BASE_PATH = "/api";
+  public final static String ADMIN_BASE_PATH = BASE_PATH + "/admin";
 }
